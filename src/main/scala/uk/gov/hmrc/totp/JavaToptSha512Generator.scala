f7d8a8d04686d4788cd8a1cbc81f4f5044b872cf
Andy Spaven <andy.spaven@digital.hmrc.gov.uk>
2018-10-23 11:16:58 +0100

    API-3464 - Make java classes stand alone

--- src/main/scala/uk/gov/hmrc/totp/JavaToptSha512Generator.scala
+++ src/main/scala/uk/gov/hmrc/totp/JavaToptSha512Generator.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package uk.gov.hmrc.totp
 
-class JavaTotpGenerator extends Totp
-
-
+class JavaToptSha512Generator extends TotpSha512
